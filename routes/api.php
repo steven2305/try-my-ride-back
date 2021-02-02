c5931297189d3df296b8f45026b69fe402d6fb72
steven patiño <spatino.gu@gmail.com>
2021-02-02 13:22:54 -0500

    functions

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register',[AuthController::class, 'register']);
+
+Route::post('/login',[AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+
+    Route::post('/logout',[AuthController::class, 'logout']);
+
 });
